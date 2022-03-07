089f84559cef6998a46949184ee91afe8c54de89
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-03-07 20:06:23 +0530

    most functionalities done

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ const divs = () => {
         document.getElementById("main-div").appendChild(elem);
     }
 }
+
+const color1 = (el) => {
+    root = document.querySelector(":root");
+    root.style.setProperty("--first", el.value);
+}
+const color2 = (el) => {
+    root = document.querySelector(":root");
+    root.style.setProperty("--second", el.value);
+}
+
+const play = () => {
+    root = document.querySelector(":root");
+    root.style.setProperty("--anim", "color_change");
+}
+
+const pause = () => {
+    root = document.querySelector(":root");
+    root.style.setProperty("--anim", "none");
+}
+
+const timer = (el) => {
+    if (el.value > 3)
+        el.value = 3;
+    root = document.querySelector(":root");
+    root.style.setProperty("--time", el.value + "s");
+}
